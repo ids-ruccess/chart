fe946f6e1aa0edf7ef3f5edc66ba5f7279cd3b76
ids-ruccess <ruccess@idstrust.com>
2025-04-06 12:08:35 +0900

    feature: GetIt interface 주입

--- lib/features/home/presentation/providers/chart_provider.dart
+++ lib/features/home/presentation/providers/chart_provider.dart
@@ -3,7 +3,6 @@ import 'package:chart/features/home/domain/repositories/chart_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../../injection_container.dart';
 import '../../domain/entities/chart_data.dart';
-import '../../domain/repositories/chart_repository_impl.dart';
 import '../../domain/usecases/fetch_chart_data.dart';
 
 // 차트 레파지토리 프로바이더
@@ -13,8 +12,7 @@ final chartRepositoryProvider = Provider<ChartRepository>((ref) {
 
 // 차트 데이터 프로바이더
 final fetchChartDataProvider = FutureProvider<ChartData>((ref) async {
-  final repository = ref.watch(chartRepositoryProvider);
-  final fetchChartData = FetchChartData(repository);
+  final fetchChartData = getIt<FetchChartData>(); // DI된 인스턴스를 사용
   return await fetchChartData();
 });
 
